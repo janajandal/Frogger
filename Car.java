b6a85f452ef737fe8369ce45ca4e491c4cc5737f
Jana Jandal Alrifai <46036668+janajandal@users.noreply.github.com>
2019-12-29 13:10:02 -0500

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,14 +1,16 @@
-public class Car {
+public class Obstacle {
     //pic/type are either going to be ints or pics depending on how we want to code
     private static int x,y,dir,speed;
+    private static String type;
     private static final int LEFT = -1, RIGHT = 1;
 
-    public Car(int lane){
-        if(lane==1 || lane==3 || lane==5) {
-            dir=LEFT; //y=wherever we decide these lanes are;
+    public Obstacle(int lane,String type){
+        this.type=type;
+        if((lane % 2) == 0) {
+            dir=RIGHT; //y=wherever we decide these lanes are;
             x=0;
-        } else if(lane==2 || lane==4){
-            dir=RIGHT ; //y=wherever we decide these lanes are;
+        } else{
+            dir=LEFT ; //y=wherever we decide these lanes are;
             x=0;
         }
 
