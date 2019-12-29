5cc22b92c7568fdb2c9ae1cf8e6cb4d7ffbcd035
Jana Jandal Alrifai <46036668+janajandal@users.noreply.github.com>
2019-12-29 15:50:06 -0500

    Update Frog.java

--- Frog.java
+++ Frog.java
@@ -6,20 +6,23 @@ public class Frog {
     private int lives;
     private int dir;
     private Image[]pics;
+    private int points;
     private int frame;
-    private static final int FOR = 1, BACK = -1, LEFT = -1, RIGHT = 1;
+    private static final int UP = 0, DOWN = 1, RIGHT = 2, LEFT = 3;
     public Frog() {
-        x=0;
-        y=0;
+        x=420;
+        y=450;
         frame = 0;
         lives=3;
-        pics = new Image[4];
+        pics = new Image[4][5];
         for(int i = 0; i<pics.length; i++){
-            Image img = new ImageIcon("Frog/frog"+i+".png").getImage();
-            pics[i] = img.getScaledInstance(30,50,Image.SCALE_SMOOTH);
-
+            for(int j = 0; j<5; j ++) {
+                String file = String.format("Frog/frog%d/frog%d.png", i, j);
+                Image img = new ImageIcon(file).getImage();
+                pics[i][j] = img.getScaledInstance(30,30,Image.SCALE_SMOOTH);
+            }
         }
-        dir=FOR;
+
     }
     public boolean isDead(){
         return lives<=0;
@@ -30,40 +33,43 @@ public class Frog {
     public int getX() {
         return x;
     }
-    public void setDir(int dir) {
-        this.dir = dir;
+    public void horizantalMove(int dist) {
+        if(dist < 0) {
+            dir = LEFT;
+        } else {
+            dir = RIGHT;
+        }
+        x += dist;
     }
-
-    public void Move(){ //in main we check for button , if button is left we setDir to 1, if down dir=-1, then call sidewaysMove
-        x+=dir;
+	public void verticalMove(int dist) {
+		if(dist < 0) {
+			dir = UP;
+		} else {
+			dir = DOWN;
     }
-    public void verticalMove(){ //same concept as sideways move
-        y=y+(dir*20);
+		y += dist;
     }
-    public void horizontalMove(){ //same concept as sideways move
-        x=x+(dir*20);
+    
+    public void stayStill () {
+        frame = 0;
     }
-
-
-    public void frogJump() {
-        frame = frame + 1 < pics.length ? frame + 1 : 0;
+    public void frogJump(){
+        frame = frame + 1 < pics.length ? frame + 1 : 4;
         delay(100);
-
     }
-    public void jump(Graphics g){
-        int w = pics[frame].getWidth(null);
-        int h = pics[frame].getHeight(null);
-        g.drawImage(pics[frame], x + w, y, -w, h, null);
+    public void jump (Graphics g){
+        Image sprite = pics[dir][frame];
+        g.drawImage(sprite, x + 30, y, -30, 30, null);
     }
 
-    public static void delay (long len){
-        try	{
-            Thread.sleep (len);
-        }
-        catch (InterruptedException ex)	{
-            System.out.println("I hate when my sleep is iterrupted");
+    
+    public void delay(int i) {
+        try {
+            Thread.sleep(len);
+        } catch (InterruptedException ex) {
+            System.out.println("I hate when my sleep is interrupted");
         }
     }
-
-
 }
+
+   
