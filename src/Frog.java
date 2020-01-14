ae3806d53194f055c5f7c0fb423ebc55ea9058b5
scatherine <59343226+scatherine@users.noreply.github.com>
2020-01-14 10:58:10 -0500

    Update Frog.java

--- src/Frog.java
+++ src/Frog.java
@@ -2,25 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Frog {
-    private int x,y,lives,dir,points,home;
-    private Image[][]pics;
-    private boolean lady;
-    private double frame;
-    private static final int UP = 0, DOWN = 1, RIGHT = 2, LEFT = 3;
+    private int x,y,lives,dir,points,home, col;
+	private Image[][][] frogPics;
+	private static final int UP = 0, DOWN = 1, RIGHT = 2, LEFT = 3, GREEN = 0, PURPLE = 1;
+	private double frame;
+    private boolean lady; 
+	
     public Frog(int x,int y) {
         this.x=x;
         this.y=y;
         frame = 4;
         lives=3;
-        pics = new Image[4][5];
-        for(int i = 0; i<pics.length; i++){
-            for(int j = 0; j<5; j ++) {
-                String file = String.format("Frog/frog%d/frog%d.png", i, j);
-                Image img = new ImageIcon(file).getImage();
-                pics[i][j] = img.getScaledInstance(30,30,Image.SCALE_SMOOTH);
-            }
-        }
-
+		dir = UP;
+		col = GREEN;
+		loadPics();
     }
     public boolean isDead(){
         return lives<=0;
@@ -49,7 +44,7 @@ public class Frog {
         } else {
             dir = RIGHT;
         }
-        x += dist * 30;
+        x += dist * 48;
 
     }
 	public void verticalMove(int dist) {
@@ -59,7 +54,7 @@ public class Frog {
 			dir = DOWN;
     }
 
-		y += dist * 30;
+		y += dist * 48;
     }
     public void setPoints(int points){
         this.points=points;
@@ -77,12 +72,30 @@ public class Frog {
         frame = 0;
     }
     public void frogJump(){
-        frame = frame + 0.25< pics.length ? frame + 0.25: 4;
+        frame = frame + 0.125< pics.length ? frame + 0.125: 4;
 
     }
     public void jump (Graphics g){
-        Image sprite = pics[dir][(int) Math.round(frame)];
-        g.drawImage(sprite, x, y, 30, 30, null);
+        Image sprite = frogPics[col][dir][(int) Math.round(frame)];
+        g.drawImage(sprite, x, y, 48, 48, null);
+    }
+	
+	public void loadPics() {
+		frogPics = new Image[2][4][5];
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 5; j ++) {
+				String file = String.format("green/frog%d/frog%d.png", i, j);
+				Image img = new ImageIcon(file).getImage();
+				frogPics[GREEN][i][j] = img.getScaledInstance(48,48,Image.SCALE_SMOOTH);
+			}
+		}
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 5; j ++) {
+				String file = String.format("purple/frog%d/frog%d.png", i, j);
+				Image img = new ImageIcon(file).getImage();
+				frogPics[PURPLE][i][j] = img.getScaledInstance(48,48,Image.SCALE_SMOOTH);
+			}
+		}
     }
 
 }
