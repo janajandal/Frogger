a3b228659692dee26438dbc175d003fd947b14ce
scatherine <59343226+scatherine@users.noreply.github.com>
2020-01-22 11:19:13 -0500

    Add files via upload

---
+++ src/Frog.java
@@ -0,0 +1,116 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Frog {
+	private static final int UP = 0, DOWN = 1, RIGHT = 2, LEFT = 3, GREEN = 0, PURPLE = 1, X = 0, Y = 1;
+	private int x, y, dir, col;
+	private int[] deadPos;
+	private double frame;
+	private boolean rebirth;
+	
+	private Image[][][] frogPics;
+	private Image[] deadPics, sitPics;
+	
+	
+	public Frog(int x, int y) {
+		this.x = x;
+		this.y = y;
+		dir = UP;
+		col = GREEN;
+		deadPos = new int[2];
+		rebirth = false;
+		frame = 4;
+		
+		loadPics();
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public Rectangle getRect() {
+		return new Rectangle(x, y, 38, 38);
+	}
+	
+	public boolean isDead() {
+		return deadPos[X] != 0 && deadPos[Y] != 0;
+	}
+	
+	public boolean newLife() {
+		return rebirth;
+	}
+	
+	public void horizantalMove(int dist) {
+		dir = dist < 0 ? LEFT : RIGHT;
+		x += dist*38;
+	}
+	
+	public void verticalMove(int dist) {
+		dir = dist < 0 ? UP : DOWN;
+		y += dist*38;
+	}
+	
+    public void stayStill() {
+        frame = 0;
+    }
+    public void frogJump(){
+        frame = frame + 0.125 <= 4 ? frame + 0.125 : 4;
+    }
+    
+    public void jump (Graphics g) {
+        Image sprite = frogPics[col][dir][(int)Math.round(frame)];
+        g.drawImage(sprite, x, y, 38, 38, null);
+    }
+    
+    public void frogDeath() {
+		deadPos[X] = x;
+		deadPos[Y] = y;
+		x = 600;
+		y = 0;
+		frame = 0;
+	}
+	
+	public void die(Graphics g) {
+		g.drawImage(deadPics[(int)Math.round(frame)], deadPos[X], deadPos[Y], 38, 38, null);
+		rebirth = frame == 4 ? true : false;
+	}
+    
+    public void sit(Graphics g, int column) {
+    	g.drawImage(sitPics[col], column, 64, 38, 38, null);
+	}
+
+    public void loadPics() {
+		frogPics = new Image[2][4][5];
+		deadPics = new Image[5];
+		sitPics = new Image[2];
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 5; j ++) {
+				String file = String.format("frog/green/frog%d/frog%d.png", i, j);
+				Image img = new ImageIcon(file).getImage();
+				frogPics[GREEN][i][j] = img.getScaledInstance(38 ,38 ,Image.SCALE_SMOOTH);
+			}
+		}
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 5; j ++) {
+				String file = String.format("frog/purple/frog%d/frog%d.png", i, j);
+				Image img = new ImageIcon(file).getImage();
+				frogPics[PURPLE][i][j] = img.getScaledInstance(38 ,38 ,Image.SCALE_SMOOTH);
+			}
+		}
+		for(int i = 0; i < 5; i++) {
+			String file = String.format("frog/dead/dead%d.png", i);
+			Image img = new ImageIcon(file).getImage();
+			deadPics[i] = img.getScaledInstance(38, 38, Image.SCALE_SMOOTH);
+		}
+		for(int i = 0; i < 2; i++) {
+			String file = String.format("frog/sit/sit%d.png", i);
+			Image img = new ImageIcon(file).getImage();
+			sitPics[i] = img.getScaledInstance(38, 38, Image.SCALE_SMOOTH);
+		}
+    }
+}
---
+++ src/Frogger.java
@@ -0,0 +1,189 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.io.*;
+
+
+public class Frogger extends JFrame implements ActionListener{
+	Timer myTimer;
+	GamePanel game;
+				
+	public Frogger() {
+		super("Frogger");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		myTimer = new Timer(10, this);	 // trigger every 10 ms
+		
+		game = new GamePanel(this);
+		add(game);
+		
+		pack();		
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	public void start(){
+		myTimer.start();
+	}
+
+	public void actionPerformed(ActionEvent evt) {
+		game.move();
+		game.repaint();
+	}
+
+    public static void main(String[] args) {
+		Frogger frame = new Frogger();
+	}
+}
+
+
+class GamePanel extends JPanel implements KeyListener {
+	private Frogger mainFrame;
+	private boolean[] keys;
+	private Font font;
+	private Frog player;
+	
+	private Image back;
+	private int w, h, lives;
+	
+	private ArrayList<Car> cars;
+	private ArrayList<Log> logs;
+	//private ArrayList<Turtle> turtles;
+	
+	public GamePanel(Frogger m) {
+		mainFrame = m;
+		keys = new boolean[KeyEvent.KEY_LAST+1];
+		addKeyListener(this);
+		gameFont();
+		
+		back = new ImageIcon("back.png").getImage();	//542x600
+		w = back.getWidth(this);
+		h = back.getHeight(this);
+		setPreferredSize(new Dimension(w, h));
+		
+		player = new Frog(w/2, 520);
+		
+		lives = 3;
+		
+		cars = new ArrayList<Car>();
+		logs = new ArrayList<Log>();
+		//turtles = new ArrayList<Turtle>();
+		load(1);
+	}
+	
+	public void addNotify() {
+		super.addNotify();
+		requestFocus();
+        mainFrame.start();
+	}
+	
+	public void move() {
+		if(player.getY() == 64 || player.newLife()) {
+			System.out.println("new");
+			player = new Frog(w/2, 520);
+		} else if(keys[KeyEvent.VK_UP] && player.getY() > 64){
+			player.verticalMove(-1);
+			player.stayStill();
+			keys[KeyEvent.VK_UP] = false;
+		} else if(keys[KeyEvent.VK_DOWN] && player.getY() < 558){
+			player.verticalMove(1);
+			player.stayStill();
+			keys[KeyEvent.VK_DOWN] = false;
+		} else if(keys[KeyEvent.VK_RIGHT]){
+			if(player.getY() >= 292 && player.getX() >= 490) {
+				return;
+			}
+			player.horizantalMove(1);
+			player.stayStill();
+			keys[KeyEvent.VK_RIGHT] = false;
+		} else if(keys[KeyEvent.VK_LEFT]){
+			if(player.getY() >= 292 && player.getX() <= 34) {
+				return;
+			}
+			player.horizantalMove(-1);
+			player.stayStill();
+			keys[KeyEvent.VK_LEFT] = false;
+		}
+		for(Car c : cars) {
+			c.move();
+			if(c.checkCollision(player)) {
+				lives--;
+				player.frogDeath();
+			}
+		}
+		boolean drown = true;
+		for(Log l : logs) {
+			l.move();
+			if(player.getY() < 292 && l.checkCollision(player)) {
+				drown = false;
+			}
+		}
+		if(player.getY() < 292 && drown) {
+			lives--;
+			player.frogDeath();
+		}
+		
+		player.frogJump();
+		
+		Point mouse = MouseInfo.getPointerInfo().getLocation();
+		Point offset = getLocationOnScreen();
+		if(keys[KeyEvent.VK_R]){
+            System.out.println("("+(mouse.x-offset.x)+", "+(mouse.y-offset.y)+")");
+		}
+	}
+	
+    public void load(int lvl) {
+        for(int i = 0; i < 5; i++) {
+        	cars.add(new Car(lvl, i));
+        }
+        //turtles.add(new Turtle(lvl, 0));
+        logs.add(new Log(lvl, 1));
+        logs.add(new Log(lvl, 2));
+        //turtles.add(new Turtle(lvl, 3));
+        logs.add(new Log(lvl, 4));
+    }
+    
+	public void keyTyped(KeyEvent e) {}
+
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+    }
+
+    public void keyReleased(KeyEvent e) {
+    	keys[e.getKeyCode()] = false;
+    }
+    
+    public void paint(Graphics g) {
+		g.drawImage(back, 0, 0, 542, 600, null);
+		for(Car c : cars) {
+			c.draw(g);
+		}
+		for(Log l : logs) {
+			l.draw(g);
+		}
+    	if(player.isDead()) {
+    		player.die(g);
+    	} else {
+    		player.jump(g);
+    	}
+    	g.setColor(Color.white);
+    	g.setFont(font.deriveFont(12f));
+    	g.drawString("SCORE", 5, 15);
+    	g.drawString("HIGH SCORE", 205, 15);
+    	
+    	g.setFont(font.deriveFont(18f));
+    	g.drawString("TIME", 400, 590);
+    }
+    
+    public void gameFont() {
+    	InputStream is = GamePanel.class.getResourceAsStream("frogger_font.ttf");
+    	try {
+    		font = Font.createFont(Font.TRUETYPE_FONT, is);
+    	} catch(IOException ex) {
+    		System.out.println(ex);	
+    	} catch(FontFormatException ex) {
+    		System.out.println(ex);	
+    	}
+    }
+}
