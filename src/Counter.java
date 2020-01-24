dd2eb952ac67e3b1ba80c5b8d95cdd61c56d7b86
Jana Jandal Alrifai <46036668+janajandal@users.noreply.github.com>
2020-01-24 13:27:44 -0500

    Comments

--- src/Counter.java
+++ src/Counter.java
@@ -1,19 +1,22 @@
+//FILENAME:Counter.java
+//BY: Jana Jandal Alrifai and Catherine Sun
+//SUMMARY: used to keep track of time in Frogger
 public class Counter {
     private int len, current;
     
     public Counter(int len) {
-        this.len = len;
+        this.len = len; //length of seconds needed multiplied by 10
     }
     
     public void countDown() {
-        current = len--;
+        current = len--; //counts down the seconds
     }
     
     public String toString() {
-        return (String.valueOf(current));
+        return (String.valueOf(current)); //used to display the time
     }
     
-    public int left(){
+    public int left(){ //checks how many seconds are left to be used for points
         return current;
     }
 }
